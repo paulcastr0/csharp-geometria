bd99fa4c75586ae68f1146179567c77f59b585db
paulcastr0 <castropaul.learn@gmail.com>
2022-11-25 16:06:24 +0100

    Completamento Esercizio

--- Geometria-CSharp/Class1.cs
+++ Geometria-CSharp/Class1.cs
@@ -5,9 +5,34 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace Geometria_CSharp
+
+// Definisco una nuova classe per poter istanziare e lavorare con oggetti di tipo Rettangolo
 {
     internal class Rettangolo
     {
-        
+        //CARATTERISTICHE / PROPRIETA'  
+        public int basee;
+        public int altezza;
+
+        //METODI
+        public int AreaRettangolo()
+        {
+            int area = basee * altezza;
+            return area;
+        }
+
+        public int PerimetroRettangolo()
+        {
+            int perimetro = (basee + altezza) * 2;
+            return perimetro;
+        }
+
+        public void infoRettangolo()
+        {
+            Console.WriteLine("Base:\t" + basee);
+            Console.WriteLine("Altezza:\t" + altezza);
+            Console.WriteLine("Perimetro:\t" + PerimetroRettangolo());
+            Console.WriteLine("Area:\t" + AreaRettangolo());
+        }
     }
 }
--- Geometria-CSharp/Program.cs
+++ Geometria-CSharp/Program.cs
@@ -1,2 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using Geometria_CSharp;
+
+Rettangolo rettangolo1 = new Rettangolo();
+
+Console.Write("La base del rettangolo è: ");
+rettangolo1.basee = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("L'altezza del rettangolo è: ");
+rettangolo1.altezza = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Rettangolo 1:");
+rettangolo1.infoRettangolo();
 
